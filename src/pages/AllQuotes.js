8ca7ee525edbe6d5f78450d178a6cb652ea9fffc
Shahbaaz <shahbaazc@gmail.com>
2022-03-11 20:26:31 +0530

    Adding Dummy Data & More Content

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,7 +1,16 @@
+import {Fragment} from 'react';
+
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_LIST = [
+    { id: 'q1', author: 'React is Good for learnig', text: 'React JS'},
+    { id: 'q2', author: 'React is Good for Work', text: 'React Route'},
+    { id: 'q3', author: 'React is Good for taching', text: 'React Redx'},
+];
 const AllQuotes = () => {
-    return <div>
-        All Quotes
-    </div>
-}
+  return <Fragment>
+      <QuoteList quotes={DUMMY_LIST} />
+  </Fragment>;
+};
 
 export default AllQuotes;
